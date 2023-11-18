a135fc1fef29a7775e13c6c514247e13eb77f3ff
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-18 14:48:37 -0500

    sign up api routing

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"; // importing from 'default export', we can change name
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 // CONNECTION
 const PORT = process.env.PORT || 4001;
@@ -19,3 +21,4 @@ app.listen(PORT, () => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
