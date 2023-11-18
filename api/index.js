7815fdcad08f585796796fab829b9e2332fc69aa
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-18 15:53:29 -0500

    create middleware

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"; // importing from 'default export', we can change name
 import authRouter from "./routes/auth.route.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.json());
 // CONNECTION
 const PORT = process.env.PORT || 4001;
 const MONGO_URI = process.env.MONGODB_URI;
+// console.log(PORT);
+// console.log(MONGO_URI);
 mongoose
     .connect(MONGO_URI)
     .then(() => console.log("Connected to mongodb"))
@@ -22,3 +25,15 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// app.use((err, req, res, next) => {
+//     const statusCode = err.statusCode || 500;
+//     const message = err.message || "Internal Server Error";
+//     return res.status(statusCode).json({
+//         success: false,
+//         statusCode,
+//         message,
+//     });
+// });
+
+app.use(errorHandler);
