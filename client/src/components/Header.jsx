3eb02aadca9e5e1337d2899574f63d522b4f6ee0
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-19 20:57:03 -0500

    display google profile image and private route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+    const { currentUser } = useSelector((state) => state.user);
+
     return (
         <header className="bg-slate-200 shadow-md">
             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,11 +33,21 @@ export default function Header() {
                             About
                         </li>
                     </Link>
-                    <Link to="/sign-in">
-                        <li className="sm:inline text-slate-400 hover:text-slate-600 hover:underline cursor-pointer">
-                            Sign in
-                        </li>
-                    </Link>
+                    {currentUser ? (
+                        <Link to="/profile">
+                            <img
+                                className="rounded-full h-7 w-7 object-cover"
+                                src={currentUser.avatar}
+                                alt="profile"
+                            />
+                        </Link>
+                    ) : (
+                        <Link to="/sign-in">
+                            <li className="sm:inline text-slate-400 hover:text-slate-600 hover:underline cursor-pointer">
+                                Sign in
+                            </li>
+                        </Link>
+                    )}
                 </ul>
             </div>
         </header>
