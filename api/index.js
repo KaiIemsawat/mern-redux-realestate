758484b2236e5ca1791e150f4badd2edc6577461
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-20 21:31:05 -0500

    api, update user function

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import userRouter from "./routes/user.route.js"; // importing from 'default export', we can change name
 import authRouter from "./routes/auth.route.js";
 import { catchError } from "./middleware/errorMiddleware.js";
@@ -8,6 +10,7 @@ import { catchError } from "./middleware/errorMiddleware.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 // CONNECTION
 const PORT = process.env.PORT || 4001;
