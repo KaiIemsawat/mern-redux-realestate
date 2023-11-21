0aaaf7423efb3391bd6cd9fa73b39b554da24c1c
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-20 20:43:44 -0500

    upload function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,55 @@
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+    getStorage,
+    getDownloadURL,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
     const { currentUser } = useSelector((state) => state.user);
+    const fileRef = useRef(null);
+    const [file, setFile] = useState(undefined);
+    const [filePercentage, setFilePercentage] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [formData, setFormData] = useState({});
+
+    console.log(formData);
+    console.log(filePercentage);
+    console.log(fileUploadError);
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                // console.log("Upload is " + progress + "% complete");
+                setFilePercentage(Math.round(progress));
+            },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setFormData({ ...FormData, avatar: downloadURL });
+                });
+            }
+        );
+    };
 
     return (
         <div className="p-3 max-w-lg mx-auto">
@@ -9,11 +57,34 @@ export default function Profile() {
                 Profile
             </h1>
             <form className="flex flex-col gap-4">
+                <input
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                    ref={fileRef}
+                    hidden
+                    accept="image/*"
+                />
                 <img
                     className="rounded-full h-24 w-24 object-cover cursor-pointer self-center my-4"
-                    src={currentUser.avatar}
+                    onClick={() => fileRef.current.click()}
+                    src={formData.avatar || currentUser.avatar}
                     alt="avatar"
                 />
+                <p className="self-center">
+                    {fileUploadError ? (
+                        <span className="text-red-600 mt-2">
+                            File Upload Error. Image can't be larger than 2mb
+                        </span>
+                    ) : filePercentage > 0 && filePercentage < 100 ? (
+                        <span className="text-slate-600 mt-2">{`Uploading...${filePercentage}%`}</span>
+                    ) : filePercentage === 100 ? (
+                        <span className="text-green-600 mt-2">
+                            Successfully Loaded
+                        </span>
+                    ) : (
+                        ""
+                    )}
+                </p>
                 <input
                     className="border p-3 rounded-lg"
                     type="text"
@@ -45,3 +116,13 @@ export default function Profile() {
         </div>
     );
 }
+
+/* firebase storage roles */
+// service firebase.storage {
+//     match /b/{bucket}/o {
+//       match /{allPaths=**} {
+//         allow read;
+//         allow write: if request.resource.size < 2 * 1024 * 1024 && request.resource.contentType.matches('image/.*');
+//       }
+//     }
+// }
