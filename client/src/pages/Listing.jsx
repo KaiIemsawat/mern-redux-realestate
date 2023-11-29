7e3fc7dbc00e6ac79702bb99b8bea32e67494aeb
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-28 20:31:39 -0500

    add swiper (carousel?)

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
 
 export default function Listing() {
+    SwiperCore.use([Navigation]);
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(false);
@@ -39,6 +44,23 @@ export default function Listing() {
                 <p className="text-center text-red-600 text-sm my-8">
                     Something went wrong...
                 </p>
+            )}
+            {listing && !loading && !error && (
+                <>
+                    <Swiper navigation>
+                        {listing.imageUrls.map((url) => (
+                            <SwiperSlide key={url}>
+                                <div
+                                    className="h-[550px]"
+                                    style={{
+                                        background: `url(${url}) center no-repeat`,
+                                        backgroundSize: "cover",
+                                    }}
+                                ></div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </>
             )}
         </main>
     );
