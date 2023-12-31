2056197d57132096138aea4868b2b3da43ca0937
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-31 14:24:27 -0500

    errorMiddleware

---
+++ recreate-project/api/middleware/errorHandle.js
@@ -0,0 +1,22 @@
+export const errorMiddleware = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    const message = err.message || "Internal Server Error";
+
+    if (err.name === "CastError" && err.kind === "ObjectId") {
+        statusCode = 404;
+        message = "Resource not found";
+    }
+
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+};
+
+export const errorHandle = (statusCode, message) => {
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+};
--- recreate-project/api/server.js
+++ recreate-project/api/server.js
@@ -5,6 +5,7 @@ import momgoose from "mongoose";
 import connectDB from "./connection/db.js";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import { errorMiddleware } from "./middleware/errorHandle.js";
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,8 @@ app.use(express.json()); // Needed in order to send json
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
     console.log(`SERVER HAS LAUNCH ON PORT :: ${PORT} ::`);
