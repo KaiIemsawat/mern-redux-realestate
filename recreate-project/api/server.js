01c86ad3e69b401af5cda8dfc69ccdeaef803919
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-31 09:55:34 -0500

    test route

--- recreate-project/api/server.js
+++ recreate-project/api/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import momgoose from "mongoose";
 
 import connectDB from "./connection/db.js";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,8 @@ connectDB();
 const PORT = process.env.PORT || 3300;
 
 const app = express();
+
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
     console.log(`SERVER HAS LAUNCH ON PORT :: ${PORT} ::`);
