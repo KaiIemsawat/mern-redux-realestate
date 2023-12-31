16db8bda00ad85a397b7ee120d1e9b88f1c66f87
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-31 13:59:18 -0500

    base signup

--- recreate-project/api/server.js
+++ recreate-project/api/server.js
@@ -4,6 +4,7 @@ import momgoose from "mongoose";
 
 import connectDB from "./connection/db.js";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 connectDB();
@@ -11,8 +12,10 @@ connectDB();
 const PORT = process.env.PORT || 3300;
 
 const app = express();
+app.use(express.json()); // Needed in order to send json
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
     console.log(`SERVER HAS LAUNCH ON PORT :: ${PORT} ::`);
