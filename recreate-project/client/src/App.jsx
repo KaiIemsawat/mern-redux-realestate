a7c58586ac926d4d919810d895ebec4afbf0f805
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-30 22:30:52 -0500

    added pages

--- recreate-project/client/src/App.jsx
+++ recreate-project/client/src/App.jsx
@@ -1,4 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
 const App = () => {
-    return <h1 className="text-red-500">App</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<Signin />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 export default App;
