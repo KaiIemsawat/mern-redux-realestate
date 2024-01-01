a49dcef25edc9eb824d551a5ab8c95fe2b1da6aa
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-31 23:09:01 -0500

    created userSlice

---
+++ recreate-project/client/src/redux/user/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false,
+};
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        signInFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+    },
+});
+
+//  When call it, it's "action"
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+//  When change it, it's "reducer"
+export default userSlice.reducer;
