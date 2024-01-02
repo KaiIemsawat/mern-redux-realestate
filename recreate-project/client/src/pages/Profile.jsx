0f6630bb2da95140e6d6e26d682855c018145717
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-01 20:32:31 -0500

    upload file feature.

--- recreate-project/client/src/pages/Profile.jsx
+++ recreate-project/client/src/pages/Profile.jsx
@@ -1,20 +1,113 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+
+import { app } from "../firebase.js";
 
 const Profile = () => {
     const { currentUser } = useSelector((state) => state.user);
+    const [file, setFile] = useState(undefined);
+    const [uploadPercentage, setUploadPercentage] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [formData, setFormData] = useState({});
+    const [showSuccessfulMsg, setShowSuccessfulMsg] = useState(false);
+
+    const fileRef = useRef(null);
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app); // 'app' comes from 'firebase.js'
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const upLoadTask = uploadBytesResumable(storageRef, file);
+
+        upLoadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setUploadPercentage(Math.round(progress));
+            },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(upLoadTask.snapshot.ref).then((downloadURL) => {
+                    setFormData({ ...formData, avatar: downloadURL });
+                });
+            }
+        );
+    };
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
+    useEffect(() => {
+        if (uploadPercentage === 100) {
+            setShowSuccessfulMsg(true);
+
+            const timeout = setTimeout(() => {
+                setShowSuccessfulMsg(false);
+            }, 5000);
+            return () => clearTimeout(timeout);
+        }
+    }, [uploadPercentage]);
+
     return (
         <div className="p-3 max-w-lg mx-auto">
-            <div className="flex justify-between mt-7 mb-4 items-center">
-                <img
-                    className="rounded-full h-24 w-24 object-cover cursor-pointer"
-                    src={currentUser.avatar}
-                    alt="profile img"
-                />
-                <h1 className="text-3xl text-end font-semibold text-primary-500">
-                    {currentUser.username}
-                </h1>
-            </div>
             <form className="flex flex-col gap-4">
+                <div className="flex justify-between mt-7 mb-4 items-center">
+                    <div className="flex gap-3 items-center">
+                        {/* Hidden input that will take effect when image is clicked  */}
+                        <input
+                            onChange={(e) => setFile(e.target.files[0])}
+                            // [0] to allow only one (first) file
+                            type="file"
+                            ref={fileRef}
+                            hidden
+                            accept="image/*"
+                        />
+                        <img
+                            className="rounded-full h-24 w-24 object-cover cursor-pointer"
+                            src={currentUser.avatar}
+                            alt="profile img"
+                            onClick={() => fileRef.current.click()}
+                        />
+                        <p>
+                            {fileUploadError ? (
+                                <>
+                                    <p className="text-error">
+                                        Image uploading error.
+                                    </p>
+                                    <p className="text-error">
+                                        Please try another file.
+                                    </p>
+                                </>
+                            ) : uploadPercentage > 0 &&
+                              uploadPercentage < 100 ? (
+                                <span className="text-effect-500">
+                                    {uploadPercentage} % upload
+                                </span>
+                            ) : (
+                                showSuccessfulMsg && (
+                                    <span className="text-effect-500">
+                                        File successfully uploaded
+                                    </span>
+                                )
+                            )}
+                        </p>
+                    </div>
+                    <h1 className="text-3xl text-end font-semibold text-primary-500">
+                        {currentUser.username}
+                    </h1>
+                </div>
                 <input
                     className="border p-3 rounded-lg focus:outline-none"
                     id="username"
@@ -49,3 +142,9 @@ const Profile = () => {
     );
 };
 export default Profile;
+
+/* firebase storage rules */
+// allow read;
+// allow write: if
+// request.resource.size < 2 * 1024 * 1024 &&
+// request.resource.contentType.matches('image/.*')
