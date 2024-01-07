9d3592f035b35ca4b55d25fe698fa6b2e4573d24
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-07 09:42:09 -0500

    search function backend

--- recreate-project/api/controllers/listing.controller.js
+++ recreate-project/api/controllers/listing.controller.js
@@ -74,3 +74,53 @@ export const getListing = async (req, res, next) => {
         next(error);
     }
 };
+
+// Get all listing and logics for search function
+export const gatAllListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let offer = req.query.offer;
+        if (offer === undefined || offer === "false") {
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === "false") {
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === "false") {
+            parking = { $in: [false, true] };
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === "all") {
+            type = { $in: ["sale", "rent"] };
+        }
+
+        const searchTerm = req.query.searchTerm || "";
+
+        const sort = req.query.sort || "createdAt";
+
+        const order = req.query.order || "desc";
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: "i" },
+            // $regex: searchTerm -> mongo search function term. $options : "i" -> ignore case
+            offer,
+            furnished,
+            parking,
+            type,
+        })
+            .sort({ [sort]: order })
+            .limit(limit)
+            .skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+};
--- recreate-project/api/routes/listing.route.js
+++ recreate-project/api/routes/listing.route.js
@@ -5,6 +5,7 @@ import {
     deleteListing,
     updateListing,
     getListing,
+    gatAllListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -14,5 +15,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.put("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", gatAllListings);
 
 export default router;
