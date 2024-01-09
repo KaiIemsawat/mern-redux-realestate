10eb7f5453e45e874bf7608e702f28e3a390a8c8
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-08 20:33:45 -0500

    more typo fixing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Signin from "./pages/Singin";
+import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
