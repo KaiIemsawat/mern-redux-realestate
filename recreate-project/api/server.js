60f311822a61aff4de76ffb18ace1d717451c78d
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-08 19:55:44 -0500

    setup for deployment

--- recreate-project/api/server.js
+++ recreate-project/api/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import momgoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import connectDB from "./connection/db.js";
 import userRouter from "./routes/user.route.js";
@@ -14,6 +15,8 @@ connectDB();
 
 const PORT = process.env.PORT || 3300;
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json()); // Needed in order to send json
 app.use(cookieParser());
@@ -21,6 +24,12 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(errorMiddleware);
 
