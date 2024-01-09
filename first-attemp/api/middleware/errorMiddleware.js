40133b399573e5820191d58f401872f2828b1007
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-08 20:02:54 -0500

    relocated directories for deployment

--- first-attemp/api/middleware/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const catchError = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
-};
